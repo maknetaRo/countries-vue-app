62c6145114fca07a81f2f3a98bdba43e3842982a
maknetaRo <makneta@gmail.com>
2021-02-18 20:36:41 +0100

    started vue app with rest countries api

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <all-countries-list></all-countries-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AllCountriesList from "./components/AllCountriesList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AllCountriesList,
+  },
+};
 </script>
 
 <style>
@@ -21,7 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/AllCountriesList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>Countries List</h1>
+    <table style="width: 100%">
+      <tr>
+        <th>name</th>
+        <th>capital city</th>
+        <th>flag</th>
+        <th>population</th>
+        <th>timezone</th>
+        <th>languages</th>
+        <th>currencies</th>
+      </tr>
+      <tr v-for="country in countries" :key="country.name">
+        <td>{{ country.name }}</td>
+        <td>{{ country.capital }}</td>
+        <td>{{ country.flag }}</td>
+        <td>{{ country.population }}</td>
+        <td>{{ country.timezones[0] }}</td>
+        <td>{{ country.languages[0].name }}</td>
+        <td>{{ country.currencies[0].code }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "all-countries-list",
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((response) => {
+        this.countries = response.data;
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style scoped>
+tr {
+  padding: 1.5rem;
+}
+th,
+td {
+  padding: 0.25rem;
+}
+th {
+  background: lime;
+}
+tr:nth-of-type(2n) {
+  background: lightgoldenrodyellow;
+}
+</style>
